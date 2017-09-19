b681d0a124b067748bd803d25065677eb0039d69
lamter <lamter.fu@gmail.com>
2017-09-19 11:28:23 +0800

    - change : 更改构建镜像的路径

--- slave.turtle/build_docker_more.sh
+++ slave.turtle/build_docker_more.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
+cd ..
 docker rmi vnpy:cta
-docker build --force-rm -t vnpy:cta .
+docker -f ./savle.turtle/Dockerfile build --force-rm -t vnpy:cta .
 
 # 删除虚悬镜像
 docker rmi $(docker images -q -f dangling=true)
