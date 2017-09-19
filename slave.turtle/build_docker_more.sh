d8646fc54e0165f87d407daddcfab8f81166a0c6
lamter <lamter.fu@gmail.com>
2017-09-19 12:27:07 +0800

    - change : 完成重新构建脚本

--- slave.turtle/build_docker_more.sh
+++ slave.turtle/build_docker_more.sh
@@ -1,8 +1,7 @@
 #/bin/bash
 
 cd ..
-docker rmi vnpy:cta
-docker -f ./savle.turtle/Dockerfile build --force-rm -t vnpy:cta .
+docker build -f ./slave.turtle/Dockerfile --force-rm -t vnpy:cta .
 
 # 删除虚悬镜像
 docker rmi $(docker images -q -f dangling=true)
