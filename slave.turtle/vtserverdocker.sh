a096d73251d9b06d2d38258e9cef21b68d0ca33b
lamter <lamter.fu@gmail.com>
2017-09-19 12:50:24 +0800

    - change : 增加依赖包 arrow

--- slave.turtle/vtserverdocker.sh
+++ slave.turtle/vtserverdocker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
+# 尚未调试完成
 cd ..
 docker run --rm --net host --name vnpy_turtle \
     -v $PWD:/srv/vnpy \
     -v /var/log/svnpy:/var/log/svnpy \
-    vnpy:cta --entrypoint ./vtserver.sh
+    vnpy:cta ./vtserver.sh
