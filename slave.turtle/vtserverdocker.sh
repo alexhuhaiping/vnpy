6824791579276cf3a3d6dc51573501dc23962520
lamter <lamter.fu@gmail.com>
2017-09-19 12:41:52 +0800

    - add : 增加 docker 下的启动脚本

--- slave.turtle/vtserverdocker.sh
+++ slave.turtle/vtserverdocker.sh
@@ -4,4 +4,4 @@ cd ..
 docker run --rm --net host --name vnpy_turtle \
     -v $PWD:/srv/vnpy \
     -v /var/log/svnpy:/var/log/svnpy \
-    vnpy:cta /bin/bash
+    vnpy:cta --entrypoint ./vtserver.sh
