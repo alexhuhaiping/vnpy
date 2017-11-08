e4e2e4f57bd84977b0c932cc84718595712983fe
lamter <lamter.fu@gmail.com>
2017-11-08 23:42:59 +0800

    - change : 在 web engine 添加 Log

--- vnpy/trader/app/webUI/webEngine.py
+++ vnpy/trader/app/webUI/webEngine.py
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 
+import traceback
 from flask import Flask
 from threading import Thread
 import json
@@ -50,22 +51,29 @@ def showCtaStrategy():
         assert isinstance(ctaApp, CtaEngine)
 
     html = ''
-    for ctaName, ctaStrategy in ctaApp.strategyDict.items():
-        html += ctaName
-        html += '</br>'
-        html += ctaStrategy.className
-        html += '</br>'
-        for index, data in ctaStrategy.toHtml().items():
-            if isinstance(data, dict):
-                html += pd.DataFrame([data], index=[index]).to_html()
-            else:
-                html += data
-
+    try:
+        for ctaName, ctaStrategy in ctaApp.strategyDict.items():
+            html += ctaName
             html += '</br>'
-            # html += pd.DataFrame([ctaStrategy.paramList2Html()], index=['param']).to_html()
-            # html += pd.DataFrame([ctaStrategy.varList2Html()], index=['var']).to_html()
+            html += ctaStrategy.className
+            html += '</br>'
+            for index, data in ctaStrategy.toHtml().items():
+                if isinstance(data, dict):
+                    html += pd.DataFrame([data], index=[index]).to_html()
+                else:
+                    html += data
+
+                html += '</br>'
+                # html += pd.DataFrame([ctaStrategy.paramList2Html()], index=['param']).to_html()
+                # html += pd.DataFrame([ctaStrategy.varList2Html()], index=['var']).to_html()
+            html += '</br>'
+            html += '</br>'
+    except:
+        err = traceback.format_exc()
         html += '</br>'
         html += '</br>'
+        html += err.replace('\n', '</br>')
+        print(err)
     return html
 
 
