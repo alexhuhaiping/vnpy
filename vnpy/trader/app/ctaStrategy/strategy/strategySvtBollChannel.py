10a017456cf89abfdddd77f38cc0cd9f5b4f4296
lamter <lamter.fu@gmail.com>
2017-11-17 21:53:13 +0800

    - change : 去掉 onBar 的日志

--- vnpy/trader/app/ctaStrategy/strategy/strategySvtBollChannel.py
+++ vnpy/trader/app/ctaStrategy/strategy/strategySvtBollChannel.py
@@ -152,8 +152,6 @@ class SvtBollChannelStrategy(CtaTemplate):
     def onBar(self, bar):
         """收到Bar推送（必须由用户继承实现）"""
         self.bm.updateBar(bar)
-        if self.trading:
-            self.log.info(u'更新 bar {}'.format(bar.datetime))
 
     # ----------------------------------------------------------------------
     def onXminBar(self, xminBar):
