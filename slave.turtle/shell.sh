cf6672411c815226445abd57d0c7d0dd4c13383e
lamter <lamter.fu@gmail.com>
2017-11-17 23:23:44 +0800

    - change : 不更改 shell.sh

--- slave.turtle/shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker rm -f vnpyshell
-cd ..
-docker run --rm -it --name vnpyshell \
-    -v $PWD:/srv/vnpy \
-    -v /private/var/log/svnpy:/var/log/svnpy \
-    -p 8080:8080 \
-    vnpy:cta /bin/bash
