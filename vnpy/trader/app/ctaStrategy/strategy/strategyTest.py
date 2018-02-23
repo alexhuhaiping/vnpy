30237986293b98ccce3556674ac5db593c05110e
lamter <lamter.fu@gmail.com>
2018-02-23 14:52:13 +0800

    feat: 增加一个测试用的策略

---
+++ vnpy/trader/app/ctaStrategy/strategy/strategyTest.py
@@ -0,0 +1,216 @@
+# encoding: UTF-8
+
+"""
+调试用的策略
+"""
+
+from __future__ import division
+
+from collections import OrderedDict
+
+from vnpy.trader.vtFunction import exception
+from vnpy.trader.vtConstant import *
+from vnpy.trader.vtObject import VtTradeData
+from vnpy.trader.app.ctaStrategy.ctaTemplate import (BarManager, ArrayManager)
+from vnpy.trader.app.ctaStrategy.svtCtaTemplate import CtaTemplate
+
+OFFSET_CLOSE_LIST = (OFFSET_CLOSE, OFFSET_CLOSETODAY, OFFSET_CLOSEYESTERDAY)
+
+
+########################################################################
+class TestStrategy(CtaTemplate):
+    """测试用的策略"""
+    className = 'TestStrategy'
+    author = u'lamter'
+
+    hands = 1
+
+    # 策略参数
+
+    # 参数列表，保存了参数的名称
+    paramList = CtaTemplate.paramList[:]
+    paramList.extend([
+        'hands',
+    ])
+
+    # 变量列表，保存了变量的名称
+    varList = CtaTemplate.varList[:]
+    _varList = [
+    ]
+    varList.extend(_varList)
+
+    @exception('raise')
+    def __init__(self, ctaEngine, setting):
+        """Constructor"""
+        super(TestStrategy, self).__init__(ctaEngine, setting)
+
+    def initMaxBarNum(self):
+        self.maxBarNum = 60
+
+    # ----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
+    @exception('raise')
+    def onInit(self):
+        """初始化策略（必须由用户继承实现）"""
+        self.writeCtaLog(u'%s策略初始化' % self.name)
+
+        # 载入历史数据，并采用回放计算的方式初始化策略数值
+        initData = self.loadBar(self.maxBarNum)
+
+        self.log.info(u'即将加载 {} 条 bar 数据'.format(len(initData)))
+
+        self.initContract()
+
+        for bar in initData:
+            self.bm.bar = bar
+            # TOOD 测试代码
+            self.tradingDay = bar.tradingDay
+            self.onBar(bar)
+            self.bm.preBar = bar
+
+        # self.log.info(u'加载的最后一个 bar {}'.format(bar.datetime))
+
+        if len(initData) >= self.maxBarNum:
+            self.log.info(u'初始化完成')
+        else:
+            self.log.info(u'初始化数据不足!')
+
+        # 从数据库加载策略数据
+        if not self.isBackTesting():
+            # 需要等待保证金加载完毕
+            document = self.fromDB()
+            self.loadCtaDB(document)
+
+        self.isCloseoutVaild = True
+
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    @exception('raise')
+    def onStart(self):
+        """启动策略（必须由用户继承实现）"""
+        self.log.info(u'%s策略启动' % self.name)
+
+        if not self.isBackTesting():
+            # 实盘，可以存库。
+            self.saving = True
+
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    @exception('raise')
+    def onStop(self):
+        """停止策略（必须由用户继承实现）"""
+        self.log.info(u'%s策略停止' % self.name)
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    @exception('raise')
+    def onTick(self, tick):
+        """收到行情TICK推送（必须由用户继承实现）"""
+        if self.trading:
+            self.bm.updateTick(tick)
+
+    # ----------------------------------------------------------------------
+    @exception('raise')
+    def onBar(self, bar):
+        """
+        self.bar 更新完最后一个 tick ，在生成新的 bar 之前将 self.bar 传入
+        该函数是由下一根 bar 的第一个 tick 驱动的，而不是当前 bar 的最后一个 tick
+        :param bar:
+        :return:
+        """
+        self.bm.updateXminBar(bar)
+        if self.isCloseoutVaild and self.rtBalance < 0:
+            # 爆仓，一键平仓
+            self.closeout()
+
+    # ----------------------------------------------------------------------
+    @exception('raise')
+    def onXminBar(self, xminBar):
+        """
+        这个函数是由 self.xminBar 的最后一根 bar 驱动的
+        执行完这个函数之后，会立即更新到下一个函数
+        :param xminBar:
+        :return:
+        """
+        bar = xminBar
+
+        # 全撤之前发出的委托
+        self.cancelAll()
+
+        # 保存K线数据
+        am = self.am
+
+        am.updateBar(bar)
+
+        if not am.inited:
+            return
+
+        # 计算指标数值
+        self.ma = am.ma(5)
+
+        if self.trading:
+            self.orderOnXminBar(bar)
+
+        # 发出状态更新事件
+        self.saveDB()
+        self.putEvent()
+        self.log.info(u'更新 XminBar {}'.format(xminBar.datetime))
+
+    def onOrder(self, order):
+        """收到委托变化推送（必须由用户继承实现）"""
+        pass
+
+    # ----------------------------------------------------------------------
+    @exception()
+    def onTrade(self, trade):
+        assert isinstance(trade, VtTradeData)
+
+        originCapital = preCapital = self.capital
+
+        self.charge(trade.offset, trade.price, trade.volume)
+
+        # 手续费
+        charge = preCapital - self.capital
+
+        preCapital = self.capital
+
+        # 回测时滑点
+        if self.isBackTesting():
+            self.chargeSplipage(trade.volume)
+
+        # 计算成本价和利润
+        self.capitalBalance(trade)
+        profile = self.capital - preCapital
+
+        if not self.isBackTesting():
+            # if self.isBackTesting():
+            textList = [u'{}{}'.format(trade.direction, trade.offset)]
+            textList.append(u'资金变化 {} -> {}'.format(originCapital, self.capital))
+            textList.append(u'仓位{} -> {}'.format(self.prePos, self.pos))
+            textList.append(u'手续费 {} 利润 {}'.format(round(charge, 2), round(profile, 2)))
+            textList.append(
+                u','.join([u'{} {}'.format(k, v) for k, v in self.positionDetail.toHtml()])
+            )
+
+            self.log.warning(u'\n'.join(textList))
+        if self.isBackTesting():
+            if self.capital <= 0:
+                # 回测中爆仓了
+                self.capital = 0
+
+        # 成交后重新下单
+
+        self.orderOnXminBar(self.xminBar)
+
+        # 发出状态更新事件
+        self.saveDB()
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onStopOrder(self, so):
+        """停止单推送"""
+
+        self.putEvent()
