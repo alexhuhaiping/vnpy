3dcfb20ec253bf481a71042d4df3c29bb2adec99
lamter <lamter.fu@gmail.com>
2018-04-24 09:34:30 +0800

    fix: 关闭容器时错误的荣启明

--- optization/kill_optboss.sh
+++ optization/kill_optboss.sh
@@ -5,6 +5,6 @@ docker kill -s SIGTERM vnpy_optboss && echo '发出终止信号'
 # 等待操作完成
 sleep 2
 # 强制关闭容器
-docker rm -f vnpy_opt && echo '关闭容器'
+docker rm -f vnpy_optboss && echo '关闭容器'
 # 关闭网络路由
 docker network disconnect --force host vnpy_optboss
--- optization/kill_optserver.sh
+++ optization/kill_optserver.sh
@@ -5,6 +5,6 @@ docker kill -s SIGTERM vnpy_optserver && echo '发出终止信号'
 # 等待操作完成
 sleep 2
 # 强制关闭容器
-docker rm -f vnpy_opt && echo '关闭容器'
+docker rm -f vnpy_optserver && echo '关闭容器'
 # 关闭网络路由
 docker network disconnect --force host vnpy_optserver
