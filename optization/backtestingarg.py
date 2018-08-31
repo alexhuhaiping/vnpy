5336f1e580c3bc0274bcd571abe2ad5e6140a8dc
lamter <lamter.fu@gmail.com>
2018-08-31 10:57:24 +0800

    feat: 更改批量回测参数

--- optization/backtestingarg.py
+++ optization/backtestingarg.py
@@ -347,12 +347,12 @@ class BacktestingArg(object):
 
 if __name__ == '__main__':
     # 本机测试配置
-    # argFileName = 'opt_test.json'
-    # optfile = 'optimize.ini'
+    argFileName = 'opt_test.json'
+    optfile = 'optimize.ini'
 
     # home 配置
-    argFileName = '/Users/lamter/workspace/SlaveO/svnpy/optization/opt_ContrarianAtrStrategy.json'
-    optfile = 'optimizeHome.ini'
+    # argFileName = '/Users/lamter/workspace/SlaveO/svnpy/optization/opt_ContrarianAtrStrategy.json'
+    # optfile = 'optimizeHome.ini'
 
     print(u'即将使用 {} 的配置'.format(optfile))
     time.sleep(5)
