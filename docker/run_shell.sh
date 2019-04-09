596de5b3e81f099ccd71f7a19c1de64a4841272a
lamter <lamter.fu@gmail.com>
2019-04-09 13:51:14 +0800

    feat: 对构建文件进行更多分层

--- docker/run_shell.sh
+++ docker/run_shell.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 # 以 shell 方式启动容器，环境如同 Linux 命令行
 
+docker rm -f vnpy_shell
 cd ..
-
-## Run docker container, mapping the ssh and vnc ports, then run bash terminal:
-docker run --name vnpy_bash --rm \
-
-    -v `pwd`:/srv/vnpy \
-    -p 2222:22 \
-    -it vnpy:latest \
-    /bin/bash
-
+# 运行 Docker 容器
+docker run --rm --name vnpy_shell \
+    -v $PWD:/srv/vnpy \
+    -v /private/var/log/svnpy:/var/log/svnpy \
+    -it vnpy /bin/bash
